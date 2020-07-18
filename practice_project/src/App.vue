93d67ce590d422789958b93bb45f50ae12df2cd0
ryusuke06 <or.06.012@gmail.com>
2020-07-18 22:48:36 +0900

    vueプロジェクトでコンポーネント間の値やりとりをしてみる

---
+++ practice_project/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <!-- v-bindディレクティブで属性の値を通して子コンポーネントに値を渡す -->
+    <HelloWorld v-bind:title="message"
+    v-on:result-event="appAction" />
+    <!-- 子コンポーネントから値を受け取る
+    v-onディレクティブでresult-eventイベントにappActionメソッドをバインド
+      流れは「$emitでイベント発火→イベントが発生し、メソッドを呼び出す」 -->
+    <hr>
+    <p>{{ result }}</p>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data: function(){
+    return {
+      message: 'HELLO',
+      result: 'no event.',
+    };
+  },
+  methods:{
+    appAction: function(message) {
+      this.result = '(*** you send :"' + message + '". ***)';
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ practice_project/src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    <h1>{{ title }}</h1>
+    <p>{{ message }}</p>
+    <hr>
+    <div>
+      <input type="text" v-model="input">
+      <button v-on:click="doAction">Click</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    title: String
+  },
+  data:function(){
+    return {
+      message: 'お名前は？',
+      input: 'no name',
+    };
+  },
+  methods:{
+    doAction: function(){
+      this.message = 'こんにちは、' + this.input + 'さん！';
+      this.$emit('result-event', this.input);
+      //$emit(イベント,引数)で親コンポーネントに引数を渡す
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
